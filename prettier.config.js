a92169ea5973978b04bdd92bfdd27f5bdcb17c80
dingruoqian99 <ding.ruoqian@synapxe.sg>
2025-06-27 23:54:49 +0800

    feat(config): standardize monorepo ESLint and Prettier configuration
    - Add root-level prettier.config.js with Tailwind CSS plugin
    - Consolidate prettier dependencies at monorepo root
    - Standardize ESLint configs to use shared @repo/eslint-config
    - Update apps/web to use @repo/eslint-config/react-internal
    - Add apps/lambda/eslint.config.js using @repo/eslint-config/base
    - Add lint script to lambda app for consistency
    - Remove redundant prettier dependencies from individual apps

---
+++ prettier.config.js
@@ -0,0 +1,10 @@
+/** @type {import('prettier').Config} */
+export default {
+  plugins: ['prettier-plugin-tailwindcss'],
+  semi: true,
+  trailingComma: 'es5',
+  singleQuote: false,
+  printWidth: 80,
+  tabWidth: 2,
+  useTabs: false,
+};
