c83f4f5d04452d54e3100deca2c7949d303eee5d
dingruoqian99 <ding.ruoqian@synapxe.sg>
2025-07-14 15:58:20 +0800

    feat: add TmaHomeRoute to default as the routing page for tma routes

---
+++ apps/web/src/routes/_tma/home.tsx
@@ -0,0 +1,29 @@
+import { Navigate, createFileRoute } from "@tanstack/react-router";
+import useStartParams from "@/hooks/useStartParams";
+
+export const Route = createFileRoute("/_tma/home")({
+  component: TmaIndexRoute,
+});
+
+function TmaIndexRoute() {
+  const tmaStartParams = useStartParams();
+  const { chat_id, chat_type } = tmaStartParams ?? {};
+
+  const chatId = chat_id ?? 0;
+  const chatType = chat_type ?? "private";
+
+  // Only redirect to the /chat/$chatId route if it is a group chat and currently on /_tma/chat
+  if (chatType !== "private") {
+    return (
+      <Navigate
+        to="/chat/$chatId"
+        params={{ chatId: chatId.toString() }}
+        search={{
+          title: "👥 Group",
+        }}
+      />
+    );
+  }
+
+  return <Navigate to="/chat" />;
+}
