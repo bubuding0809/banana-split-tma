c07e71fdf36a7fe303a1937fbda7089b147c26fd
dingruoqian99 <ding.ruoqian@synapxe.sg>
2025-07-14 16:07:56 +0800

    refactor: simplify ChatIndexRoute by removing unused imports and logic

--- apps/web/src/routes/_tma/chat.tsx
+++ apps/web/src/routes/_tma/chat.tsx
@@ -1,34 +1,9 @@
-import {
-  Navigate,
-  Outlet,
-  createFileRoute,
-  useLocation,
-} from "@tanstack/react-router";
-import useStartParams from "@/hooks/useStartParams";
+import { Outlet, createFileRoute } from "@tanstack/react-router";
 
 export const Route = createFileRoute("/_tma/chat")({
   component: ChatIndexRoute,
 });
 
 function ChatIndexRoute() {
-  const location = useLocation();
-  const tmaStartParams = useStartParams();
-  const { chat_id, chat_type } = tmaStartParams ?? {};
-
-  const chatId = chat_id ?? 0;
-  const chatType = chat_type ?? "private";
-
-  // Only redirect to the /chat/$chatId route if it is a group chat and currently on /_tma/chat
-  if (chatType !== "private" && location.pathname === Route.fullPath) {
-    return (
-      <Navigate
-        to="/chat/$chatId"
-        params={{ chatId: chatId.toString() }}
-        search={{
-          title: "👥 Group",
-        }}
-      />
-    );
-  }
   return <Outlet />;
 }
