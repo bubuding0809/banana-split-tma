e18a69f8e52873156b1cf0dab853988008a95fee
dingruoqian99 <ding.ruoqian@synapxe.sg>
2025-07-15 15:35:50 +0800

    feat: add loading skeletons and flag emojis for currency selection in CurrencyNavList

--- apps/web/src/components/features/Chat/CurrencyNavList.tsx
+++ apps/web/src/components/features/Chat/CurrencyNavList.tsx
@@ -1,7 +1,7 @@
 import { trpc } from "@/utils/trpc";
 import { getRouteApi } from "@tanstack/react-router";
 import { initData, useSignal } from "@telegram-apps/sdk-react";
-import { Chip, Radio } from "@telegram-apps/telegram-ui";
+import { Chip, Radio, Skeleton } from "@telegram-apps/telegram-ui";
 import { useEffect } from "react";
 
 const routeApi = getRouteApi("/_tma/chat/$chatId");
@@ -19,10 +19,13 @@ const CurrencyNavList = () => {
   const { data: chatData } = trpc.chat.getChat.useQuery({
     chatId: Number(chatId),
   });
-  const { data: currencies } = trpc.currency.getCurrenciesWithBalance.useQuery({
-    userId,
-    chatId,
-  });
+  const { data: supportedCurrencies } =
+    trpc.currency.getSupportedCurrencies.useQuery({});
+  const { data: currencies, status: getCurrenciesStatus } =
+    trpc.currency.getCurrenciesWithBalance.useQuery({
+      userId,
+      chatId,
+    });
 
   // Default selected currency to base currency
   useEffect(() => {
@@ -46,7 +49,22 @@ const CurrencyNavList = () => {
   };
 
   return (
-    <nav className="no-scrollbar flex gap-x-2 overflow-x-auto px-4 py-2">
+    <nav className="no-scrollbar flex gap-x-2 overflow-x-auto px-4 py-1">
+      {getCurrenciesStatus === "pending" &&
+        Array.from({ length: 5 }).map((_, index) => (
+          <Chip
+            key={index}
+            before={
+              <Skeleton visible={getCurrenciesStatus === "pending"}>
+                🌏
+              </Skeleton>
+            }
+          >
+            <Skeleton visible={getCurrenciesStatus === "pending"}>
+              <span className="text-xl">SGD</span>
+            </Skeleton>
+          </Chip>
+        ))}
       {currencies?.map((currency) => (
         <Chip
           key={currency.code}
@@ -59,6 +77,13 @@ const CurrencyNavList = () => {
       ))}
       {currencies?.length === 0 && (
         <Chip
+          after={
+            <span className="text-xl">
+              {supportedCurrencies?.find(
+                (c) => c.code === chatData?.baseCurrency
+              )?.flagEmoji ?? "🌏"}
+            </span>
+          }
           before={
             <Radio checked={selectedCurrency === chatData?.baseCurrency} />
           }
