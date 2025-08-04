8b912e8700ba496320215e046dac9e77efa220e4
dingruoqian99 <ding.ruoqian@synapxe.sg>
2025-08-04 16:02:28 +0800

    feat: enhance Vite configuration to load environment variables and manage allowed hosts

--- apps/web/vite.config.ts
+++ apps/web/vite.config.ts
@@ -1,27 +1,34 @@
 import { tanstackRouter } from "@tanstack/router-plugin/vite";
-import { defineConfig } from "vite";
+import { defineConfig, loadEnv } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import tailwindcss from "@tailwindcss/vite";
 
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [
-    tanstackRouter({
-      target: "react",
-      autoCodeSplitting: true,
-    }),
-    react(),
-    tailwindcss(),
-  ],
-  resolve: {
-    alias: {
-      "@": "/src",
-      "@components": "/src/components",
-      "@utils": "/src/utils",
-      "@hooks": "/src/hooks",
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, "./");
+  const allowedHosts = env.VITE_ALLOWED_HOSTS
+    ? env.VITE_ALLOWED_HOSTS.split(",")
+    : [];
+
+  return {
+    plugins: [
+      tanstackRouter({
+        target: "react",
+        autoCodeSplitting: true,
+      }),
+      react(),
+      tailwindcss(),
+    ],
+    resolve: {
+      alias: {
+        "@": "/src",
+        "@components": "/src/components",
+        "@utils": "/src/utils",
+        "@hooks": "/src/hooks",
+      },
     },
-  },
-  server: {
-    allowedHosts: ["9091cea46c14.ngrok-free.app"],
-  },
+    server: {
+      allowedHosts,
+    },
+  };
 });
