63da3d330fb981341a701ca0d82d6d2125d627d0
dingruoqian99 <ding.ruoqian@synapxe.sg>
2025-08-08 11:11:16 +0800

    feat: Implement updateUser functionality with input validation and error handling

--- packages/trpc/src/routers/user/index.ts
+++ packages/trpc/src/routers/user/index.ts
@@ -1,7 +1,9 @@
 import createUser from "./createUser.js";
 import getUser from "./getUser.js";
+import updateUser from "./updateUser.js";
 
 export const userRouter = {
   getUser,
   createUser,
+  updateUser,
 };
