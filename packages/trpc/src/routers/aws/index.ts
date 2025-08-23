3fef749802c8027f34805d1b883b9221e3b273b5
dingruoqian99 <ding.ruoqian@synapxe.sg>
2025-08-24 00:48:17 +0800

    feat: remove eventbridgeSanityCheck handler from awsRouter

--- packages/trpc/src/routers/aws/index.ts
+++ packages/trpc/src/routers/aws/index.ts
@@ -1,5 +1,4 @@
 import { createTRPCRouter } from "../../trpc.js";
-import eventbridgeSanityCheck from "./eventbridgeSanityCheck.js";
 import createRecurringSchedule from "./createRecurringSchedule.js";
 import createGroupReminderSchedule from "./createGroupReminderSchedule.js";
 import updateGroupReminderSchedule from "./updateGroupReminderSchedule.js";
@@ -7,7 +6,6 @@ import deleteGroupReminderSchedule from "./deleteGroupReminderSchedule.js";
 import getChatSchedule from "./getChatSchedule.js";
 
 export const awsRouter = createTRPCRouter({
-  eventbridgeSanityCheck,
   createRecurringSchedule,
   createGroupReminderSchedule,
   updateGroupReminderSchedule,
