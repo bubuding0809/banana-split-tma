5632eb28d76f7c1b94a394f3e3e8dc4f4b959948
Ding Ruoqian <bubuding0809@gmail.com>
2025-09-17 20:06:51 +0800

    feat: add getMultipleRates handler to currency router

--- packages/trpc/src/routers/currency/index.ts
+++ packages/trpc/src/routers/currency/index.ts
@@ -1,11 +1,13 @@
 import { createTRPCRouter } from "../../trpc.js";
 import getCurrentRate from "./getCurrentRate.js";
+import getMultipleRates from "./getMultipleRates.js";
 import refreshRates from "./refreshRates.js";
 import getSupportedCurrencies from "./getSupportedCurrencies.js";
 import getCurrenciesWithBalance from "./getCurrenciesWithBalance.js";
 
 export const currencyRouter = createTRPCRouter({
   getCurrentRate,
+  getMultipleRates,
   refreshRates,
   getSupportedCurrencies,
   getCurrenciesWithBalance,
