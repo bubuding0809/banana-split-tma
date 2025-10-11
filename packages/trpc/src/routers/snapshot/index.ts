21c8c58ac46a985e8500034123f014bac447f826
Ding Ruoqian <bubuding0809@gmail.com>
2025-10-11 16:38:18 +0800

    ✨ feat: implement update snapshot functionality with validation

--- packages/trpc/src/routers/snapshot/index.ts
+++ packages/trpc/src/routers/snapshot/index.ts
@@ -3,10 +3,12 @@ import createSnapshot from "./createSnapshot.js";
 import getSnapshots from "./getSnapshots.js";
 import getSnapshotDetails from "./getSnapshotDetails.js";
 import deleteSnapshot from "./deleteSnapshot.js";
+import updateSnapshot from "./updateSnapshot.js";
 
 export const snapshotRouter = createTRPCRouter({
   create: createSnapshot,
   getByChat: getSnapshots,
   getDetails: getSnapshotDetails,
   delete: deleteSnapshot,
+  update: updateSnapshot,
 });
