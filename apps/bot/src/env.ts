a5ac09dfd4f2d24ec0008dfa1a27327aa7f7bb77
Ding Ruoqian <ding.ruoqian@synapxe.sg>
2026-03-24 09:22:48 +0800

    feat(bot): setup core bot instance and env validation

---
+++ apps/bot/src/env.ts
@@ -0,0 +1,17 @@
+import { createEnv } from "@t3-oss/env-core";
+import { z } from "zod";
+import * as dotenv from "dotenv";
+
+dotenv.config();
+
+export const env = createEnv({
+  server: {
+    TELEGRAM_BOT_TOKEN: z.string().min(1),
+    NODE_ENV: z
+      .enum(["development", "test", "production"])
+      .default("development"),
+    VERCEL_URL: z.string().optional(),
+  },
+  runtimeEnv: process.env,
+  emptyStringAsUndefined: true,
+});
---
+++ apps/bot/src/types.ts
@@ -0,0 +1,5 @@
+import { Context } from "grammy";
+
+export interface BotContext extends Context {
+  // We will add trpc later
+}
