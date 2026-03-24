646d764f338f556bacf4c2752b280edf6a692089
Ding Ruoqian <ding.ruoqian@synapxe.sg>
2026-03-24 13:44:47 +0800

    feat(bot): add request logger middleware

--- apps/bot/src/bot.ts
+++ apps/bot/src/bot.ts
@@ -2,12 +2,14 @@ import { Bot } from "grammy";
 import { env } from "./env.js";
 import { BotContext } from "./types.js";
 import { trpcMiddleware } from "./middleware/trpc.js";
+import { loggerMiddleware } from "./middleware/logger.js";
 import { userFeature } from "./features/user.js";
 import { groupFeature } from "./features/group.js";
 import { expensesFeature } from "./features/expenses.js";
 
 export const bot = new Bot<BotContext>(env.TELEGRAM_BOT_TOKEN);
 
+bot.use(loggerMiddleware);
 bot.use(trpcMiddleware);
 bot.use(userFeature);
 bot.use(groupFeature);
