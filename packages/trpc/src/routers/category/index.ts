b32e606f1e8701b9d190cbae1ef99a940056b133
bubuding0809 <bubuding0809@gmail.com>
2026-04-20 18:42:01 +0800

    feat(categories): trpc category.suggest with rate limit

--- packages/trpc/src/routers/category/index.ts
+++ packages/trpc/src/routers/category/index.ts
@@ -2,6 +2,7 @@ import listByChat from "./listByChat.js";
 import createChatCategory from "./createChatCategory.js";
 import updateChatCategory from "./updateChatCategory.js";
 import deleteChatCategory from "./deleteChatCategory.js";
+import suggestCategory from "./suggestCategory.js";
 import { createTRPCRouter } from "../../trpc.js";
 
 export const categoryRouter = createTRPCRouter({
@@ -9,4 +10,5 @@ export const categoryRouter = createTRPCRouter({
   create: createChatCategory,
   update: updateChatCategory,
   delete: deleteChatCategory,
+  suggest: suggestCategory,
 });
