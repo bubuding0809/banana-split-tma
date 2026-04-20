5778a5324b542dd855e0440d323e03b756cc3df6
bubuding0809 <bubuding0809@gmail.com>
2026-04-20 18:28:56 +0800

    feat(categories): trpc category.delete with expense null-out

--- packages/trpc/src/routers/category/index.ts
+++ packages/trpc/src/routers/category/index.ts
@@ -1,10 +1,12 @@
 import listByChat from "./listByChat.js";
 import createChatCategory from "./createChatCategory.js";
 import updateChatCategory from "./updateChatCategory.js";
+import deleteChatCategory from "./deleteChatCategory.js";
 import { createTRPCRouter } from "../../trpc.js";
 
 export const categoryRouter = createTRPCRouter({
   listByChat,
   create: createChatCategory,
   update: updateChatCategory,
+  delete: deleteChatCategory,
 });
