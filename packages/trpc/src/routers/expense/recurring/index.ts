ccdc36f97c6d0e066a070cab1bfd64a410576026
bubuding0809 <bubuding0809@gmail.com>
2026-04-24 19:06:07 +0800

    ✨ feat(trpc): add expense.recurring.update mutation

--- packages/trpc/src/routers/expense/recurring/index.ts
+++ packages/trpc/src/routers/expense/recurring/index.ts
@@ -2,9 +2,11 @@ import { createTRPCRouter } from "../../../trpc.js";
 import list from "./list.js";
 import get from "./get.js";
 import cancel from "./cancel.js";
+import update from "./update.js";
 
 export const recurringRouter = createTRPCRouter({
   list,
   get,
+  update,
   cancel,
 });
